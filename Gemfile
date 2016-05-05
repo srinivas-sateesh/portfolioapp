d6d774d335c5a01e1d0a4bf2517e050a9b9955b8
srinivas-sateesh <srinivas.sateesh@gmail.com>
2016-05-05 13:16:50 +0000

    Added devise and styled forms

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'will_paginate'
 
 gem 'mail_form'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'contacts/new'
-
-  get 'contacts/create'
+  devise_for :users
+   
 
  resources :posts
  resources :projects
  resources :contacts, only: [:new, :create] 
  get 'welcome/index'
  root 'welcome#index'
-
+ get '*path' => redirect('/')
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
